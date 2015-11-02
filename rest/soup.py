9175310996f6315d436ec517873b15ed4a801ba8
Yasmeen Roumie <y4smeen@gmail.com>
2015-11-02 13:12:13 -0500

    yolo

---
+++ rest/soup.py
@@ -0,0 +1,10 @@
+import urllib2
+from bs4 import BeautifulSoup
+
+url = "http://newyorktimes.com"
+request = urllib2.urlopen(url)
+result - request.read()
+soup = BeautifulSoup(result, 'html.parser')
+#print soup.prettify()
+for r in soup.find_all("h2"):
+    print r
