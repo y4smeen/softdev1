6b8be1f87532fc42a862d86d4e686b6bb2d21ee3
Yasmeen Roumie <y4smeen@gmail.com>
2015-11-04 13:10:43 -0500

    Update getpage.py

--- rest/getpage.py
+++ rest/getpage.py
@@ -1,9 +1,17 @@
 import urllib2
+import json
 
 url = "http://www.nytimes.com"
 
 request = urllib2.urlopen(url)
 result = request.read()
+r = json.loads(result)
 
-print result
+
+print r
+print 
+print r.keys()
+print r['main']
+print r['main']['temp']
+#print result
 
